86bfbf95d777b803c0ab1106b58dbb113349a48a
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2023-08-25 23:06:00 +0300

    json news, TODO: Upload json, news page

--- src/components/home/news/HomeNews.tsx
+++ src/components/home/news/HomeNews.tsx
@@ -1,41 +1,36 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 import samsung from '../../../assets/samsung.webp';
 import xiaomi from '../../../assets/xiaomi.png';
 import { StyledTitle } from '../../../shared/styled/styled';
+import NewsItem from '../../shared/NewsItem';
 
 export default function News() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   return (
     <NewsWrapper>
       <NewsHeader>
         <StyledTitle>{t('home.news.title')}</StyledTitle>
-        <HeaderButton>{t('home.news.button')}</HeaderButton>
+        <HeaderButton onClick={() => navigate('/news')}>
+          {t('home.news.button')}
+        </HeaderButton>
       </NewsHeader>
       <NewsBody>
-        <NewsItem>
-          <ImgContainer>
-            <img src={samsung} alt="az" />
-          </ImgContainer>
-          <NewsInfo>
-            <NewsDate>19.08.2023</NewsDate>
-            <NewsTitle>{t('news.samsung.title')}</NewsTitle>
-            <NewsText>{t('news.samsung.text')}</NewsText>
-            <NewsButton>{t('news.button')}</NewsButton>
-          </NewsInfo>
-        </NewsItem>
-        <NewsItem>
-          <ImgContainer>
-            <img src={xiaomi} alt="az" />
-          </ImgContainer>
-          <NewsInfo>
-            <NewsDate>18.08.2023</NewsDate>
-            <NewsTitle>{t('news.xiaomi.title')}</NewsTitle>
-            <NewsText>{t('news.xiaomi.text')}</NewsText>
-            <NewsButton>{t('news.button')}</NewsButton>
-          </NewsInfo>
-        </NewsItem>
+        <NewsItem
+          img={samsung}
+          date="19.08.2023"
+          title={t('news.samsung.title')}
+          text={t('news.samsung.text')}
+        />
+        <NewsItem
+          img={xiaomi}
+          date="22.08.2023"
+          title={t('news.xiaomi.title')}
+          text={t('news.xiaomi.text')}
+        />
       </NewsBody>
     </NewsWrapper>
   );
@@ -60,83 +55,11 @@ const NewsBody = styled.div`
   }
 `;
 
-const NewsItem = styled.div`
-  display: flex;
-  border-radius: 20px;
-  border: 1px solid #c8c8c8;
-  padding: 20px;
-  flex: 1;
-  @media (max-width: 617px) {
-    flex-direction: column;
-    align-items: center;
-    padding: 10px;
-  }
-`;
-
 const HeaderButton = styled.button`
   color: #1b5a7d;
   font-size: 20px;
   font-weight: 500;
   &:hover {
     text-decoration: underline;
-  }
-`;
-
-const ImgContainer = styled.div`
-  min-width: 300px;
-  img {
-    border-radius: 20px;
-  }
-  @media (max-width: 617px) {
-    width: 270px;
-  }
-`;
-
-const NewsInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const NewsDate = styled.div`
-  border-radius: 20px;
-  border: 1px solid #d4d4d4;
-  padding: 5px 18px;
-  color: #1b5a7d;
-  font-size: 20px;
-  font-weight: 500;
-  align-self: flex-start;
-`;
-
-const NewsTitle = styled.div`
-  color: #1b5a7d;
-  font-size: 22px;
-  font-weight: 500;
-  margin-top: 8px;
-  line-height: 27px;
-`;
-
-const NewsText = styled.div`
-  margin-top: 8px;
-  color: #003f62;
-  font-size: 13px;
-  font-weight: 400;
-`;
-
-const NewsButton = styled.button`
-  margin-top: 15px;
-  border-radius: 20px;
-  border: 1px solid #b5b5b5;
-  padding: 10px 20px;
-  color: #1b5a7d;
-  font-size: 16px;
-  font-weight: 500;
-  align-self: flex-end;
-  &:hover {
-    background: #1b5a7d;
-    color: white;
-  }
-  @media (max-width: 555px) {
-    padding: 5px 10px;
   }
 `;
---
+++ src/components/shared/NewsItem.tsx
@@ -0,0 +1,99 @@
+import { useTranslation } from 'react-i18next';
+import { styled } from 'styled-components';
+
+type Props = {
+  img: string;
+  title: string;
+  text: string;
+  date: string;
+};
+
+export default function NewsItem({ img, title, text, date }: Props) {
+  const { t } = useTranslation();
+
+  return (
+    <NewsWrapper>
+      <ImgContainer>
+        <img src={img} alt="news" />
+      </ImgContainer>
+      <NewsInfo>
+        <NewsDate>{date}</NewsDate>
+        <NewsTitle>{title}</NewsTitle>
+        <NewsText>{text}</NewsText>
+        <NewsButton>{t('news.button')}</NewsButton>
+      </NewsInfo>
+    </NewsWrapper>
+  );
+}
+
+const NewsWrapper = styled.div`
+  display: flex;
+  border-radius: 20px;
+  border: 1px solid #c8c8c8;
+  padding: 20px;
+  flex: 1;
+  @media (max-width: 617px) {
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+  }
+`;
+
+const ImgContainer = styled.div`
+  min-width: 300px;
+  img {
+    border-radius: 20px;
+  }
+  @media (max-width: 617px) {
+    width: 270px;
+  }
+`;
+
+const NewsInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const NewsDate = styled.div`
+  border-radius: 20px;
+  border: 1px solid #d4d4d4;
+  padding: 5px 18px;
+  color: #1b5a7d;
+  font-size: 20px;
+  font-weight: 500;
+  align-self: flex-start;
+`;
+
+const NewsTitle = styled.div`
+  color: #1b5a7d;
+  font-size: 22px;
+  font-weight: 500;
+  margin-top: 8px;
+  line-height: 27px;
+`;
+
+const NewsText = styled.div`
+  margin-top: 8px;
+  color: #003f62;
+  font-size: 13px;
+  font-weight: 400;
+`;
+
+const NewsButton = styled.button`
+  margin-top: 15px;
+  border-radius: 20px;
+  border: 1px solid #b5b5b5;
+  padding: 10px 20px;
+  color: #1b5a7d;
+  font-size: 16px;
+  font-weight: 500;
+  align-self: flex-end;
+  &:hover {
+    background: #1b5a7d;
+    color: white;
+  }
+  @media (max-width: 555px) {
+    padding: 5px 10px;
+  }
+`;
